76756a194f31ae2e4c8f470c28d3c6b60841cf0b
alejandro <jose.reyes@vasslatam.com>
2019-11-05 11:29:52 -0500

    arquitectura y estructuracion proyecto apolo web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlujoComponentsComponent } from './componentes/flujo-components/flujo-components.component';
+import { HomeComponentsComponent } from './componentes/home-components/home-components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlujoComponentsComponent,
+    HomeComponentsComponent
   ],
   imports: [
     BrowserModule,
