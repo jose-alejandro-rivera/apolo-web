c32e829e067ed4d04e377072a3be23e627090c14
nevardosa <danilo.sanchez@vasslatam.com>
2019-11-06 16:49:06 -0500

    Cambios para probar la carga de la data

--- src/app/componentes/home-components/home-components.component.ts
+++ src/app/componentes/home-components/home-components.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from "@angular/forms";
-
-
-
+import { HttpClient } from '@angular/common/http';
+import { CategoriasService } from '../servicios/categorias.service';
 
 class body {
   ProductName: string;
@@ -24,82 +23,22 @@ class headers {
 export class HomeComponent implements OnInit {
 	body : body[];
   headers : headers[];
-  private rowSelection;
-  private autoGroupColumnDef;
-  private domLayout;
 
-  oppoSuits: any = [];
-  filas: any = [];
 
-  columnDefs = [
-    {
-      headerName: "Seleccionar",
-      field: "athlete",
-      headerCheckboxSelection: true,
-      headerCheckboxSelectionFilteredOnly: true,
-      checkboxSelection: true,
-    },
-    {headerName: 'Make', field: 'make', sortable: true, filter: true,  resizable: true },
-    {headerName: 'Make', field: 'make', sortable: true, filter: true, resizable: true },
-    {headerName: 'Model', field: 'model', sortable: true, filter: true, resizable: true },
-    {headerName: 'Price', field: 'price', sortable: true, filter: true, resizable: true},
-    {headerName: 'Pricexxx', field: 'pricexxx', sortable: true, filter: true, resizable: true},
-];
+  categorias: any = [];
+  //filas: any = [];
 
-rowData = [
-    { checkboxSelection: true, make: 'Toyota', model: 'Celica', price: 35000, pricexxx: 20000 },
-    { checkboxSelection: true, make: 'Ford', model: 'Mondeo', price: 32000, pricexxx: 369888 },
-    { checkboxSelection: true, make: 'Porsche', model: 'Boxter', price: 72000, pricexxx: 92540000 }
 
-];
-  constructor(private fb: FormBuilder) { 
-    setTimeout (()=> {
-     // this.selectPrueba()
-    },500) 
 
-    this.rowSelection = "multiple";
-    this.autoGroupColumnDef = {
-      headerName: "Athlete",
-      field: "athlete",
-      cellRenderer: "agGroupCellRenderer",
-      cellRendererParams: { checkbox: true }
-    };
-  }
+  constructor(private http: HttpClient, private categoriasService: CategoriasService ) {   }
   
 
   ngOnInit() {
-
-  	/*this.ps
-      .getusers()
-      .subscribe((data: body[]) => {
-        console.log(data);
-      	if(data){
-            //console.log(data, 'if');
-            this.body = data;
-            this.headers = data
-
-      	}
-       
-
-    });*/
+    console.log('init del home');
+    this.categoriasService.getCategorias().subscribe((data: body[]) => {
+      console.log(data);
+    });
 
   }
 
- /* selectPrueba(){
-    this.cf
-    .getusers()
-    .subscribe((data) => {
-      this.oppoSuits = data;
-      for(let arreglo of this.oppoSuits.body){
-          this.filas.push(
-            {
-              id: arreglo.id,
-              nombre: arreglo.nombre, 
-              apellido: arreglo.apellido
-            }
-          )
-      }
-      console.log(this.filas, 'HGASHDGHASD');
-    })
-  } */
 }
--- src/app/componentes/servicios/categorias.service.ts
+++ src/app/componentes/servicios/categorias.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasService {
+  localUrl = 'http://localhost:3000/api/flujo/categorias';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getCategorias() {
+    return this.http.get(this.localUrl);
+  }
 }
+
