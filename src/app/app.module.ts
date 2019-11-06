2bcc001196056c46277dc33e2dee5f44ec582bc7
nevardosa <danilo.sanchez@vasslatam.com>
2019-11-06 11:36:01 -0500

    Se crea carpeta de servicios y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './componentes/home-components/home-components.component';
 import { FlujoComponentsComponent } from './componentes/flujo-components/flujo-components.component';
-import { HomeComponentsComponent } from './componentes/home-components/home-components.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppComponent,
+    HomeComponent,
     FlujoComponentsComponent,
-    HomeComponentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AgGridModule.withComponents([]),
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
