0ad324d2d3fab7a0b79d3b0643776e5255706daa
nevardosa <danilo.sanchez@vasslatam.com>
2019-11-07 17:49:34 -0500

    Se ajusta servicio de categorias donde se deja dinamicamente el ingreso del id

--- src/app/componentes/servicios/categorias.service.ts
+++ src/app/componentes/servicios/categorias.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
+
+/* Consume el servicio que carga las categorias */
 export class CategoriasService {
   localUrl = 'http://localhost:3000/api/flujo/categorias';
   constructor(private http: HttpClient) { }
