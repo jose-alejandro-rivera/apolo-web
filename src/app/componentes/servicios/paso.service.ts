9d4e3ca6a094bf5ccd0c150b4ca0e7b0ae99a073
nevardosa <danilo.sanchez@vasslatam.com>
2019-11-07 17:51:27 -0500

    Servicio paso

--- src/app/componentes/servicios/paso.service.ts
+++ src/app/componentes/servicios/paso.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+/* Consume el servicio de los pasos */
 export class PasoService {
+  pasosUrl = 'http://localhost:3000/api//flujo/list/';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getCategorias(id) {
+    return this.http.get(this.pasosUrl+id);
+  }
 }
