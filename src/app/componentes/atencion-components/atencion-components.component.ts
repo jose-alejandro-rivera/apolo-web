45b14db4939e03a90b1d670ce040bf8fde0241e7
angela <angmora.7@hotmail.com>
2019-11-21 12:06:44 -0500

    flujo de pasos funcionando con paso tipo desicion logica

--- src/app/componentes/atencion-components/atencion-components.component.ts
+++ src/app/componentes/atencion-components/atencion-components.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PasoService } from '../../servicios/paso.service';
 import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { EjecucionAtencionService } from '../../servicios/ejecucionAtencion.service';
+import { PasoMockService } from '../../mocks/paso.service.mock.service';
 
 const URL = 'http://localhost:8080/api/';
 
@@ -26,9 +27,15 @@ export class AtencionComponentsComponent implements OnInit {
   idFlujo: any;
   nombreFlujo: any;
 
+  info: any;
+  pasoActual: any;
+  decisionActual: any;
+  decisionSeleccionada: any;
 
-
-  constructor(private pasosFlujo: PasoService, private atencionService: EjecucionAtencionService, private router: Router
+  constructor(private pasosFlujo: PasoService,
+    private atencionService: EjecucionAtencionService,
+    private pasoMockService: PasoMockService,
+    private router: Router
 
   ) {
     this.atencionComponente = true;
@@ -36,14 +43,13 @@ export class AtencionComponentsComponent implements OnInit {
   }
   //metodo que valida el listado de los campos 
   ngOnInit() {
-    debugger;
     this.seleccion = 1;
     this.dataFlujoCat = JSON.parse(localStorage.getItem('dataFlujoCat'));
     this.idFlujo = this.dataFlujoCat.Id_Flujo;
     this.nombreFlujo = this.dataFlujoCat.NomFlujo;
     let url = URL + 'flujo/list/' + this.idFlujo;
-    this.atencionService.getData(url).subscribe((data: any) => {
 
+    this.atencionService.getData(url).subscribe((data: any) => {
       //validacion existencia de datos
       if (data) {
         this.listFlujoPaso = data;
@@ -70,6 +76,55 @@ export class AtencionComponentsComponent implements OnInit {
       }
     });
 
+    console.log("Informacion nuevo arreglo");
+    this.info = this.pasoMockService.FlujoData[0];
+    //inicializacion del paso actual o primer paso
+    this.pasoActual = this.info.Flujo[0].CodPaso_Inicial;
+    this.decisionActual = this.info.paso_cuestionario[2];
+  }
+
+  DecisionSeleccionada(value) {
+    this.decisionSeleccionada = value;
+  }
+
+  Siguiente(Id_Paso: number) {
+    //regitrar en bd y validar paso anterior
+
+    //buscar el siguiente paso
+    const siguientePaso = this.info.FlujoPasos.find(x => x.CodPaso_Origen == Id_Paso);
+    //buscar el siguiente paso
+    const actualPaso = this.info.Pasos.find(x => x.Id_Paso == Id_Paso);
+
+    //caso de paso tipo decision
+    if (this.info.Pasos.find(x => x.Id_Paso == Id_Paso).CodTipoPaso == 2) {
+      const opcionesSiguientePaso = this.info.FlujoPasos.filter(x => x.CodPaso_Origen == Id_Paso); //lista de posibles pasos siguientes
+      console.log(opcionesSiguientePaso);
+      //evaluar expresion de ejecucion para saber que paso sigue 
+      for(let op of opcionesSiguientePaso){
+        let exp = '@' + actualPaso.CodCuestionario + '.' + this.decisionActual.pasoCuestionarioCampo.Sigla + '==' + this.decisionSeleccionada;
+        if(op.ExpresionEjecucion == exp){
+          this.pasoActual = op.CodPaso_Destino;
+        }
+      }
+      //this.decisionActual = this.info.paso_cuestionario.find(x => x.paso_cuestionario.Id_Paso == Id_Paso);
+
+    } else {
+      //paso tipo actividad
+      this.pasoActual = siguientePaso.CodPaso_Destino;
+    }
+
+  }
+
+  Atras(Id_Paso: number) {
+    console.log(Id_Paso);
+    const anteriorPaso = this.info.FlujoPasos.find(x => x.CodPaso_Destino == Id_Paso).CodPaso_Origen;
+    this.pasoActual = anteriorPaso;
+  }
+
+  RegistrarAtencion() {
+    //limpiar variables 
+    this.pasoActual = 0;
+    this.decisionSeleccionada = 0;
   }
 
   cargeuPasoFlujo() {
