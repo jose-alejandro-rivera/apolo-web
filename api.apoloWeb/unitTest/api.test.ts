afc01c04f5890dccb50fa61f5ae96c90ace8309d
angela <angmora.7@hotmail.com>
2020-01-13 15:44:24 -0500

    ajustes test

--- api.apoloWeb/unitTest/api.test.ts
+++ api.apoloWeb/unitTest/api.test.ts
@@ -1,13 +1,28 @@
 import { ApiConsult } from '../src/api/apiConsult';
+import { ServicePasoMock } from '../unitTest/mocks/service.pasoMocks';
+import {Container, Scope} from 'typescript-ioc';
 //const cl = require('../src/api/apiConsult') // Link to your server file
 //const supertest = require('supertest')
 //const request = supertest(ApiConsult)
 
 
 it('Gets the test endpoint', async done => {
-    let app = new ApiConsult();
-    // Sends GET Request to /test endpoint
-    const res = await app.getCategoriasFlujo();
-    // ...
-    done();
+
+  Container.bind(ApiConsult).to(ServicePasoMock).scope(Scope.Local);
+  let serviceMock: ServicePasoMock = Container.get(ApiConsult);
+  let apiConsult: ApiConsult = Container.get(ApiConsult);
+
+  //let objectModel = chargeJsonResponse('menuResponseObj');
+  //database.setProcedureResponse(objectModel, true);
+  console.log(serviceMock.categoriasData);
+
+  let dataResponse = await apiConsult.getCategoriasFlujo();
+  console.log(dataResponse);
+
+  let app = new ApiConsult();
+  // Sends GET Request to /test endpoint
+  const res = await app.getCategoriasFlujo();
+  // ...
+  done();
   })
+
