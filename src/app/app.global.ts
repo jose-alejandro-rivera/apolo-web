e4d21919d58626baab067179fbf6d324706aad04
alejandro <jose.reyes@vasslatam.com>
2020-01-15 16:47:12 -0500

    changes for url of the serise front

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -20,8 +20,8 @@ export class AppGlobals {
         if(urlIp.indexOf("https://10.203.217.36:20203/") > -1){
             this.url ='https://10.203.217.36:20203/api/'
         }
-        if(urlIp.indexOf("https://10.203.221.51") > -1){
-            this.url ='https://10.203.221.51/api/'
+        if(urlIp.indexOf("http://10.203.221.51") > -1){
+            this.url ='http://10.203.221.51:8080/api/'
             console.log("usted se encuentra aca -- 1234")
         }
         if(urlIp.indexOf("https://apilab.telefonica.co:20203/") > -1){
