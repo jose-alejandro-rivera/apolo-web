1016624cf033fe2176c59a221ad743f6d2749dc4
alejandro <jose.reyes@vasslatam.com>
2020-01-30 16:55:50 -0500

    administracion  validacion de las ordenes

--- src/app/componentes/orden-components/orden-components.component.ts
+++ src/app/componentes/orden-components/orden-components.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import {
-  NavigationCancel,
-  Event,
-  NavigationEnd,
-  NavigationError,
-  NavigationStart,
-  Router
-} from '@angular/router';
+import { Event, Router } from '@angular/router';
+import { AppGlobals } from 'src/app/app.global';
+import { EjecucionAtencionService } from '../../servicios/ejecucionAtencion.service';
+import * as camera from 'nativescript-camera';
+
 
 /**
  * 
@@ -15,16 +12,17 @@ import {
 @Component({
   selector: 'app-orden-components',
   templateUrl: './orden-components.component.html',
-  styleUrls: ['./orden-components.component.css']
+  styleUrls: ['./orden-components.component.css'],
+  providers: [AppGlobals]
 })
 /**
  * 
  */
 export class OrdenComponentsComponent implements OnInit {
 
-    /**
-   * variable que itera el componente atencion
-   */
+  /**
+ * variable que itera el componente atencion
+ */
   componentFlujo: boolean;
   /**
    * variable que itera el componente de categorias
@@ -34,7 +32,16 @@ export class OrdenComponentsComponent implements OnInit {
   /**
    * 
    */
-  orden: any;
+  public orden: any;
+  /**
+   * 
+   */
+  public param: any;
+  /**
+   * 
+   * 
+   */
+  public URL: any;
   /**
    * 
    */
@@ -43,33 +50,57 @@ export class OrdenComponentsComponent implements OnInit {
    * 
    */
   public submitted = false;
-  
+
+  public ordenInexistente: boolean;
+  /**
+   * 
+   */
+  public mensajeOrden: any;
+  /**
+   * 
+   */
+  public ordenResponse: any;
+
+  retomaOrden: any;
+  tipoEjecucion: any;
+
 
   /**
    * 
    * @param router 
+   * @param ejecucionAtencionService 
    * @param formBuilder 
+   * @param global 
    */
   constructor(private router: Router,
-    private formBuilder: FormBuilder) {
-      this.componentFlujo = false;
-      this.componentCategoria = false;
+    private ejecucionAtencionService: EjecucionAtencionService,
+    private formBuilder: FormBuilder,
+    private global: AppGlobals) {
+    this.componentFlujo = false;
+    this.componentCategoria = false;
+    this.ordenInexistente = false;
+    this.URL = this.global.url;
     localStorage.setItem('dataFlujoOrden', '');
     this.router.events.subscribe((event: Event) => {
     });
     this.formOrden = this.formBuilder.group({
+      param: ['', Validators.required],
       orden: ['', Validators.required]
     });
   }
 
   ngOnInit() {
 
-
-
   }
 
   get f() {
     return this.formOrden.controls;
+  }
+
+  parametroSeleccion($event) {
+    debugger
+    this.param = this.formOrden.value.param;
+    this.ordenInexistente = false;
   }
 
   ingresoOrden($event) {
@@ -78,19 +109,74 @@ export class OrdenComponentsComponent implements OnInit {
       return;
     } else {
       this.orden = this.formOrden.value.orden;
-      localStorage.setItem('dataFlujoOrden', JSON.stringify(this.orden));
+      //validacion orden con atencion 
+      if (this.retomaOrden) {
 
-      this.enrutamiento();
+        let url = this.URL + 'integracion/apolo/toa/' + this.param + '/' + this.orden;
+        return this.ejecucionAtencionService.getData(url).toPromise().then(data => {
+          console.log(data + ' imprecion data')
+          let info: any = data;
+          info = info.responseToa;
+          return info;
+        }).then(data => {
+          this.ordenResponse = data;
+          this.ordenResponse.retoma = data.retoma;
+          localStorage.setItem('dataFlujoOrden', JSON.stringify(this.ordenResponse));
+          this.enrutamientoFlujo();
+        })
 
-
-      console.log("usted esta aqui app-component");
+      } else {
+        this.validacionOrden();
+      }
     }
   }
 
-  enrutamiento() {
-    this.router.navigate(['home/componet']);
-    return false;
+
+  validacionOrden() {
+    this.tipoEjecucion='orden';
+    let url = this.URL + 'integracion/apolo/toa/' + this.param + '/' + this.orden + '/' + this.tipoEjecucion;
+    return this.ejecucionAtencionService.getData(url).toPromise().then(data => {
+      console.log(data + ' imprecion data')
+      let info: any = data;
+      info = info.responseToa;
+      return info;
+    }).then(data => {
+      this.ordenResponse = data;
+      this.ordenResponse.formOrden = this.formOrden.value;
+      if (this.ordenResponse.status) {
+        if (this.ordenResponse.status === 'started') {
+          localStorage.setItem('dataFlujoOrden', JSON.stringify(this.ordenResponse));
+          this.enrutamientoHome();
+        } else {
+          this.mensajeOrden = this.global.mensajeOrdenNoIniciada;
+          this.ordenInexistente = true;
+        }
+      } else {
+        this.mensajeOrden = this.global.mensajeOrdenNoExiste;
+        this.ordenInexistente = true;
+      }
+    })
 
   }
+
+  enrutamientoHome() {
+    this.router.navigate(['home/componet']);
+    return false;
+  }
+
+  enrutamientoFlujo() {
+    this.router.navigate(['flujo/list']);
+    return false;
+  }
+
+
+
+  // onButoonTap():void{
+  //   Image
+  //   camera.requestCameraPermissions().then(
+
+
+  //   );
+  // }
 
 }
